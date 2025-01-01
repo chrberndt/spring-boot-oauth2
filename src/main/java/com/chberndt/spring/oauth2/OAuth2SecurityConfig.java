ed54c51c92bd12ef05c34a1b8f83d792645ba494
Christian Berndt <christian.berndt@liferay.com>
2025-01-01 18:58:16 +0100

    chore: remove obsolete authentication converter.

--- src/main/java/com/chberndt/spring/oauth2/OAuth2SecurityConfig.java
+++ src/main/java/com/chberndt/spring/oauth2/OAuth2SecurityConfig.java
@@ -1,24 +1,9 @@
 package com.chberndt.spring.oauth2;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @Configuration
 @EnableWebSecurity
 public class OAuth2SecurityConfig {
-
-//	@Bean
-//	public JwtAuthenticationConverter jwtAuthenticationConverter() {
-//		JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-//		grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-//		grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-//
-//		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-//		return jwtAuthenticationConverter;
-//	}
-
 }
