89e46c257ac59d9fd189c50ce8c43ea72232d92f
Christian Berndt <christian.berndt@liferay.com>
2025-01-12 22:51:23 +0100

    feat: enable cors from localhost:4200.

--- src/main/java/com/chberndt/spring/oauth2/OAuth2Controller.java
+++ src/main/java/com/chberndt/spring/oauth2/OAuth2Controller.java
@@ -2,6 +2,7 @@ package com.chberndt.spring.oauth2;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class OAuth2Controller {
 
 	@GetMapping("/api/test")
